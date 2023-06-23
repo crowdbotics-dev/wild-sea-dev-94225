7d2d71e020392bf84ae57beb2aba08967bcbd723
Crowdbotics <engineering@crowdbotics.com>
2023-06-23 23:47:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-sea-dev-94225",
+  url: "https://wild-sea-dev-94225.botics.co",
+  api: "https://wild-sea-dev-94225.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
